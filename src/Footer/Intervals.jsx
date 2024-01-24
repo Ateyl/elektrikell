fcfd66895991398afe9c7e93dbe0d8a715ff8f4d
Artjom Buzak <Atilusemi@gmail.com>
2024-01-24 21:07:45 +0200

    UI REACT+BOOTSTRAP & ACTIVE PRICE

---
+++ src/Footer/Intervals.jsx
@@ -0,0 +1,13 @@
+import Row from 'react-bootstrap/Row';
+
+function Intervals() {
+    return (
+        <>
+            <Row>
+                <Col></Col>
+            </Row>
+        </>
+    )
+}
+
+export default Intervals;
