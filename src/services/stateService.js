64b61809d7d2d35cdbd49f2cd0b96be66c1d2d66
Artjom Buzak <Atilusemi@gmail.com>
2024-02-14 20:38:14 +0200

    prebuild

--- src/services/stateService.js
+++ src/services/stateService.js
@@ -1,50 +1,70 @@
 import {
-    createAction,
-    createReducer,
-    configureStore,
-    createSlice,
-  } from "@reduxjs/toolkit";
-  import { DEFAULT_ACTIVE_BUTTON } from "../Head/constants";
-  import { getDefaultFrom, getDefaultUntil } from "../utils/dates";
-  
-  const initialState = {
-    activePrice: DEFAULT_ACTIVE_BUTTON,
-    activeHour: 1,
-  };
-  
-  const initialDateState = {
-    from: getDefaultFrom(),
-    until: getDefaultUntil(),
-  };
-  
-  export const setActivePrice = createAction("setActivePrice");
-  export const setActiveHour = createAction("setActiveHour");
-  
-  const main = createReducer(initialState, (builder) => {
-    builder
-      .addCase(setActivePrice, (state, action) => {
-        state.activePrice = action.payload;
-      })
-      .addCase(setActiveHour, (state, action) => {
-        state.activeHour = action.payload;
-      });
-  });
-  
-  const dateSlice = createSlice({
-    name: "date",
-    initialState: initialDateState,
-    reducers: {
-      setFrom: (state, action) => {
-        state.from = action.payload;
-      },
-      setUntil: (state, action) => {
-        state.until = action.payload;
-      },
+  createAction,
+  createReducer,
+  configureStore,
+  createSlice,
+} from "@reduxjs/toolkit";
+import { DEFAULT_ACTIVE_BUTTON } from "../Head/constants";
+import { getDefaultFrom, getDefaultUntil } from "../utils/dates";
+
+const initialMainState = {
+  activePrice: DEFAULT_ACTIVE_BUTTON,
+  activeHour: 1,
+  showSideBar: false,
+  errorMessage: null,
+  bestUntil: 0,
+  isLoading: true,
+};
+
+const initialDateState = {
+  from: getDefaultFrom(),
+  until: getDefaultUntil(),
+};
+
+export const setActivePrice = createAction("setActivePrice");
+export const setActiveHour = createAction("setActiveHour");
+export const setShowSideBar = createAction("setShowSideBar");
+export const setErrorMessage = createAction("setErrorMessage");
+export const setBestUntil = createAction("setBestUntil");
+export const setIsLoading = createAction("setIsLoading");
+
+const main = createReducer(initialMainState, (builder) => {
+  builder
+    .addCase(setActivePrice, (state, action) => {
+      state.activePrice = action.payload;
+    })
+    .addCase(setActiveHour, (state, action) => {
+      state.activeHour = action.payload;
+    })
+    .addCase(setShowSideBar, (state, action) => {
+      state.showSideBar = action.payload;
+    })
+    .addCase(setErrorMessage, (state, action) => {
+      state.errorMessage = action.payload;
+    })
+    .addCase(setBestUntil, (state, action) => {
+      state.bestUntil = action.payload;
+    })
+    .addCase(setIsLoading, (state, action) => {
+      state.isLoading = action.payload;
+    })
+});
+
+const dateSlice = createSlice({
+  name: "date",
+  initialState: initialDateState,
+  reducers: {
+    setFrom: (state, action) => {
+      state.from = action.payload;
     },
-  });
-  
-  export const { setFrom, setUntil } = dateSlice.actions;
-  
-  export const store = configureStore({
-    reducer: { main, date: dateSlice.reducer },
-  });
+    setUntil: (state, action) => {
+      state.until = action.payload;
+    },
+  },
+});
+
+export const { setFrom, setUntil } = dateSlice.actions;
+
+export const store = configureStore({
+  reducer: { main, date: dateSlice.reducer },
+});
