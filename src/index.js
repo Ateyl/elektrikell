4013d5a85f53e862dde2bfbaa4592a0c17a2d8b2
Artjom Buzak <Atilusemi@gmail.com>
2024-02-16 19:18:22 +0200

    beta_1.0

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/elektrikell-latest">
+  <BrowserRouter basename="/elektrikell">
     <Provider store={store} >
       <App />
     </Provider>
