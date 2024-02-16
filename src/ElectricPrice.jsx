fa75938197abe71f5749963a1afa64de72bfc379
Artjom Buzak <Atilusemi@gmail.com>
2024-02-16 19:07:15 +0200

    ghPages test

--- src/ElectricPrice.jsx
+++ src/ElectricPrice.jsx
@@ -1,70 +1,37 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import "./App.scss";
 import Container from "react-bootstrap/Container";
 import Body from "./Body";
-import Head, { DEFAULT_ACTIVE_BUTTON } from "./Head";
+import Head from "./Head";
 import Footer from "./Footer";
 import LeftSideBar from "./LeftSideBar";
-import { getDefaultFrom, getDefaultUntil } from "./utils/dates";
 import ErrorModal from "./ErrorModal";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setActiveHour } from "./services/stateService";
+import Loading from "./Loading";
+import ElectricPriceProvider from "./contexts/ElecticPriceContext";
 
 function ElecticPrice() {
+  console.log("ElecticPrice");
   const params = useParams();
-
-  const [activePrice, setActivePrice] = useState(DEFAULT_ACTIVE_BUTTON);
-  const [activeHour, setActiveHour] = useState(1);
-  const [showSideBar, setShowSideBar] = useState(false);
-  const [from, setFrom] = useState(getDefaultFrom());
-  const [until, setUntil] = useState(getDefaultUntil());
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [bestUntil, setBestUntil] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleCloseSideBar = () => setShowSideBar(false);
-  const handleOpenSideBar = () => setShowSideBar(true);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (params.hours) setActiveHour(+params.hours);
-  }, [params]);
+    if (params.hours) dispatch(setActiveHour(+params.hours));
+  }, [params, dispatch]);
 
   return (
-    <Container>
-      <Head
-        activePrice={activePrice}
-        setActivePrice={setActivePrice}
-        handleOpenSideBar={handleOpenSideBar}
-        setErrorMessage={setErrorMessage}
-      />
-      <Body
-        activeHour={activeHour}
-        from={from}
-        until={until}
-        setErrorMessage={setErrorMessage}
-        setBestUntil={setBestUntil}
-        setIsLoading={setIsLoading}
-      />
-      <Footer
-        activePrice={activePrice}
-        activeHour={activeHour}
-        setActiveHour={setActiveHour}
-        bestUntil={bestUntil}
-      />
-      <LeftSideBar
-        show={showSideBar}
-        handleClose={handleCloseSideBar}
-        from={from}
-        until={until}
-        setFrom={setFrom}
-        setUntil={setUntil}
-      />
-      <ErrorModal
-        show={!!errorMessage}
-        handleClose={() => setErrorMessage(null)}
-        errorMessage={errorMessage}
-      />
-      {isLoading && <h1>LOADING</h1>}
-    </Container>
+    <ElectricPriceProvider>
+      <Container>
+        <Head />
+        <Body />
+        <Footer />
+        <LeftSideBar />
+        <ErrorModal />
+        <Loading />
+      </Container>
+    </ElectricPriceProvider>
   );
 }
 
---
+++ src/contexts/ElecticPriceContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+export const ElectricPriceContext = createContext(null);
+
+function ElectricPriceProvider({ children }) {
+  const [averagePrice, setAveragePrice] = useState(0);
+  const [currentPrice, setCurrentPrice] = useState(0);
+
+  const value = {
+    values: {
+      averagePrice,
+      currentPrice,
+    },
+    actions: {
+      setAveragePrice,
+      setCurrentPrice,
+    },
+  };
+
+  return (
+    <ElectricPriceContext.Provider value={value}>
+      {children}
+    </ElectricPriceContext.Provider>
+  );
+}
+
+export default ElectricPriceProvider;
